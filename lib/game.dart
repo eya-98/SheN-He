43b2e2237ace1099288864a6918cf5daed8d0bb8
unknown <eya.nani@sesame.com.tn>
2021-12-18 23:46:00 +0100

    edit

--- lib/game.dart
+++ lib/game.dart
@@ -10,45 +10,76 @@ class  StartState extends State<Game> {
    @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-            children: [
-              Expanded (
-                child: Container(
-                 width: 50,
-  height: 1000,
-                decoration:  BoxDecoration(
-                borderRadius:  BorderRadius.circular(16.0),
-                color: const Color(0xFFff6666),
-                ),
-                          child: Align(
-              alignment: Alignment.bottomCenter,
-                            child: Image.asset(
-                              "assets/fem4.png",
-                              height: 1500,
-                              width: 1000,
-                            ),
+      backgroundColor: const Color(0xFFECF3F9),
+      body: Column (
+        children: [ 
+                    Expanded(
+                      child: Row(
+                           children: [ Expanded (
+                          child: Container(
+                             width: 100,
+                            height: 150,
+                            decoration: const BoxDecoration(
+                            color: Color(0xFFECF3F9)),
+                          )
+                           )
+                           ]
                             )
-                ),
-              ),Expanded (
-                child: Container(
-                 width: 50,
-                  height: 1000,
-                decoration:  BoxDecoration(
-                borderRadius:  BorderRadius.circular(16.0),
-                color: const Color(0xFF006666),
-                ),
-                child: Align(
-              alignment: Alignment.bottomCenter,
-                            child: Image.asset(
-                              "assets/fem4.png",
-                              height: 1500,
-                              width: 1000,
-                            ),
-                            )
-                ),
-              ),
+                          ),
+                   Expanded(
+                     flex: 9,
+                    child: Row(
+                    children: [
+                        Expanded (
+                          child: Container(
+                          width: 20,
+                          height: 550,       
+                          padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
+                          decoration:  BoxDecoration(
+                          borderRadius:  BorderRadius.circular(16.0),
+                          color: const Color(0xFFff6666),
+                          ),
+                              child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                                children: 
+                                  [ 
+                                    Image.asset(
+                                    "assets/fem.png",width: 300, height: 400, fit: BoxFit.contain,
+                                    ),
+                                  ]
+                                )
+                              ),
+                          ),
+                          Expanded (
+                          child: Container(
+                          width: 20,
+                          height: 550,       
+                          padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
+                          decoration:  BoxDecoration(
+                          borderRadius:  BorderRadius.circular(16.0),
+                          color: const Color(0xFF1f7a7a),
+                           ),
+                              child: Align(
+          alignment: Alignment.center,
+          child: Column(
+            
+                                children: 
+                                  [        
+                                    Image.asset(
+                                    "assets/male1.png",width: 540, height: 370, fit: BoxFit.fill,
+                                    ),
+                                  ]
+                                )
+                              ),
+                          ),
+                          )
         ],
       )
-    );
+                  )
+      ]
+      )
+        
+
+       );
 }
 }
